6d073feaf389a73118a37ed13e92a4c7b2a57cdd
dedisusanto56 <santodominggo@gmail.com>
2021-12-17 14:50:56 +0700

    tambah materi live search menggunakan jquery

---
+++ 20-live-search-menggunakan-JQuery/index.php
@@ -0,0 +1,112 @@
+<?php 
+session_start();
+
+if( !isset($_SESSION["login"]) ){
+  header("Location: login.php");
+  exit;
+}
+
+
+// menghubungkan ke function.php
+require 'function.php';
+// atua
+// include "function.php";
+
+// ambil data dari tabel mahasiswa
+//$mahasiswa = query("SELECT * FROM mahasiswa ORDER BY id DESC"); //jika ingin mengurutkan berdasarkan id dari yang terbesar ke terkecil
+//$mahasiswa = query("SELECT * FROM mahasiswa ORDER BY id ASC"); //jika ingin mengurutkan berdasarkan id dari yang terkecil ke terbesar
+//$mahasiswa = query("SELECT * FROM mahasiswa WHERE nrp ='321654987'"); //menampilkan data berdasarkann nrp tsb
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// ketika tombol cari ditekan
+if(isset($_POST["cari"])){
+  $mahasiswa = cari($_POST["keyword"]);
+}
+  // ambil data (fetch) mahasiswa dari object result menggunakan :
+  // mysqli_fetch_row()
+  // mysqli_fetch_assoc()
+  // mysqli_fetch_array()
+  // mysqli_fetch_object()
+
+  // $mhs = mysqli_fetch_row($result);
+  // var_dump($mhs);
+
+  // $mhs = mysqli_fetch_assoc($result);
+  // var_dump($mhs);
+
+  // $mhs = mysqli_fetch_array($result);
+  // var_dump($mhs);
+
+  // $mhs = mysqli_fetch_object($result);
+  // var_dump($mhs->nama);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style>
+      .loader{
+        width: 100px;
+        position :absolute;
+        top: 125px;
+        left : 300px;
+        z-index: -1;
+        background-color: red;
+        display: none;
+      }
+    </style>
+    <title>Admin</title>
+  </head>
+  <body>
+    <a href="logout.php">Log Out</a>
+    
+    <h1>Daftar Mahasiswa</h1>
+    <a href="tambah.php">Tambah Data Mahasiswa</a>
+    <br><br>
+
+    <form action="" method="post">
+      <input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian ...." autocomplete="off" id="keyword">
+      <button type="submit" name="cari" id="tombol-cari">Cari</button>
+      <img src="img/loader1.gif" class="loader">
+    </form>
+    <br>
+
+    <div id="container">
+    <table border="1" cellpadding="10" cellspacing="0">
+      <tr>
+        <th>No.</th>
+        <th>aksi</th>
+        <th>Gambar</th>
+        <th>NRP</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Jurusan</th>
+      </tr>
+      <?php $i = 1 ?>
+      <?php foreach($mahasiswa as $row) : ?>
+      <tr>
+        <td><?= $i ?></td>
+        <td>
+          <a href="ubah.php?id=<?= $row["id"] ?>">ubah</a>
+          <a href="hapus.php?id=<?= $row["id"] ?>" onclick="return confirm('yakin');">hapus</a>
+        </td>
+        <td><img src="img/<?= $row["gambar"] ?>" width="50" /></td>
+        <td><?= $row["nrp"] ?></td>
+        <td><?= $row["nama"] ?></td>
+        <td><?= $row["email"] ?></td>
+        <td><?= $row["jurusan"] ?></td>
+      </tr>
+      <?php $i++; ?>
+      <?php endforeach; ?>
+    </table>
+    </div>
+
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/script-jquery.js"></script>
+    <!-- <script src="js/script-ajax.js"></script> -->
+  </body>
+</html>
