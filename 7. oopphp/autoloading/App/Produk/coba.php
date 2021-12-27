7e1e60d2ea4a72070e1ab595da5f9a2b650a7edc
dedisusanto56 <santodominggo@gmail.com>
2021-12-27 14:50:27 +0700

    tambah materi autoloading

---
+++ 7. oopphp/autoloading/App/Produk/coba.php
@@ -0,0 +1,7 @@
+<?php 
+class coba{
+  public function __construct()
+  {
+    echo "ini adalah class coba";
+  }
+}
---
+++ 7. oopphp/autoloading/App/init.php
@@ -0,0 +1,18 @@
+<?php
+
+// cara manual
+// require_once 'Produk/InfoProduk.php';
+// require_once 'Produk/Produk.php';
+// require_once 'Produk/Komik.php';
+// require_once 'Produk/Game.php';
+// require_once 'Produk/CetakInfoProduk.php';
+
+// cara autoloading
+// spl_autoload_register(function ($class) {
+//   require_once'Produk/' . $class . '.php';
+// });
+
+// lengkapnya
+spl_autoload_register(function ($class) {
+  require_once __DIR__ . '/Produk/' . $class . '.php';
+});
