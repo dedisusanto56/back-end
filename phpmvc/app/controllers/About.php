1e48a242af3af51e11e29fab293df11e87d34ef6
dedisusanto56 <santodominggo@gmail.com>
2021-12-29 14:40:03 +0700

    MVC view

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php
 
-class About
+class About extends Controller
 {
-  public function index($nama = 'dedi', $pekerjaan = 'gamer')
+  public function index($nama = 'sandhika', $pekerjaan = 'Dosen', $umur = 32)
   {
-    echo "Halo nama saya $nama, saya seorang $pekerjaan";
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+    $data['judul'] = 'About';
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/footer');
   }
   public function page()
   {
-    echo 'About/page';
+    $data['judul'] = 'Pages';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
