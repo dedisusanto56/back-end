0c756666ce0ce2d0d835df210c2b993b394e1ee4
dedisusanto56 <santodominggo@gmail.com>
2021-12-29 15:40:54 +0700

    MVC Assets

---
+++ phpmvc/app/core/constants.php
@@ -0,0 +1,2 @@
+<?php 
+define('BASEURL', 'http://localhost/back-end/phpmvc/public');
--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,2 +1,6 @@
-<h1>About Me</h1>
-<p>Halo, Nama Saya <?= $data['nama']; ?>, umur saya <?= $data['umur'] ?>, saya adalah seorang <?= $data['pekerjaan'] ?></p>
+<div class="container">
+
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL ?>/img/profile.jpg" alt="arch linux" width="200" class="rounded-circle shadow">
+  <p>Halo, Nama Saya <?= $data['nama']; ?>, umur saya <?= $data['umur'] ?>, saya adalah seorang <?= $data['pekerjaan'] ?></p>
+</div>
